8fbbc65f521e8cefc4d2573769082811a7d9f71d
Seth Shoultes <seth@eventespresso.com>
2012-07-17 06:07:54 +0000

    Minor change to check if the members addon is installed. [touch:172]

--- templates/registration_page.php
+++ templates/registration_page.php
@@ -290,7 +290,7 @@ if (!function_exists('register_attendees')) {
 					//echo "<pre>".print_r($member_options,true)."</pre>";
                     //If enough spaces exist then show the form
                     //Check to see if the Members plugin is installed.
-                    if ( !is_user_logged_in() && get_option('events_members_active') == 'true' && ($member_only == 'Y' || $member_options['member_only_all'] == 'Y') ) {
+                    if ( !is_user_logged_in() && espresso_members_installed() == true && ($member_only == 'Y' || $member_options['member_only_all'] == 'Y') ) {
                         event_espresso_user_login();
                     } else {
                         //Serve up the registration form
