9514615235fa20e83fa7fade062844304b7c1fdf
Sidney Harrel <sidney@eventespresso.com>
2012-10-31 21:12:32 +0000

    (3.1 1579-megasoft-gateway) created files. finished init and settings [touch:1579]

---
+++ gateways/megasoft/settings.php
@@ -0,0 +1,123 @@
+<?php
+
+function event_espresso_aim_payment_settings() {
+	global $espresso_premium, $active_gateways;;
+	if (!$espresso_premium)
+		return;
+	if (isset($_POST['update_megasoft'])) {
+		$megasoft_settings['megasoft_login_id'] = $_POST['megasoft_login_id'];
+		$megasoft_settings['header'] = $_POST['header'];
+		$megasoft_settings['use_sandbox'] = empty($_POST['use_sandbox']) ? false : true;
+		$megasoft_settings['force_ssl_return'] = empty($_POST['force_ssl_return']) ? false : true;
+		$megasoft_settings['display_header'] = empty($_POST['display_header']) ? false : true;
+		update_option('event_espresso_megasoft_settings', $megasoft_settings);
+		echo '<div id="message" class="updated fade"><p><strong>' . __('Megasoft settings saved.', 'event_espresso') . '</strong></p></div>';
+	}
+	$megasoft_settings = get_option('event_espresso_megasoft_settings');
+	if (empty($megasoft_settings)) {
+		$megasoft_settings['megasoft_login_id'] = '';
+		$megasoft_settings['header'] = 'Payment Transactions by Megasoft';
+		$megasoft_settings['use_sandbox'] = false;
+		$megasoft_settings['force_ssl_return'] = false;
+		$megasoft_settings['display_header'] = false;
+		if (add_option('event_espresso_megasoft_settings', $megasoft_settings, '', 'no') == false) {
+			update_option('event_espresso_megasoft_settings', $megasoft_settings);
+		}
+	}
+
+	//Open or close the postbox div
+	if (empty($_REQUEST['deactivate_megasoft'])
+					&& (!empty($_REQUEST['activate_megasoft'])
+					|| array_key_exists('aim', $active_gateways))) {
+		$postbox_style = '';
+	} else {
+		$postbox_style = 'closed';
+	}
+	?>
+	<div class="metabox-holder">
+		<div class="postbox <?php echo $postbox_style; ?>">
+			<div title="Click to toggle" class="handlediv"><br /></div>
+			<h3 class="hndle">
+				<?php _e('Megasoft Settings', 'event_espresso'); ?>
+			</h3>
+			<div class="inside">
+				<div class="padding">
+					<?php
+					if (isset($_REQUEST['activate_megasoft']) && $_REQUEST['activate_megasoft'] == 'true') {
+						$active_gateways['aim'] = dirname(__FILE__);
+						update_option('event_espresso_active_gateways', $active_gateways);
+					}
+					if (isset($_REQUEST['deactivate_megasoft']) && $_REQUEST['deactivate_megasoft'] == 'true') {
+						unset($active_gateways['aim']);
+						update_option('event_espresso_active_gateways', $active_gateways);
+					}
+					echo '<ul>';
+					if (array_key_exists('aim', $active_gateways)) {
+						echo '<li id="deactivate_aim" style="width:30%;" onclick="location.href=\'' . get_bloginfo('wpurl') . '/wp-admin/admin.php?page=payment_gateways&deactivate_megasoft=true\';" class="red_alert pointer"><strong>' . __('Deactivate Megasoft Gateway?', 'event_espresso') . '</strong></li>';
+							event_espresso_display_megasoft_settings();
+					} else {
+						echo '<li id="activate_aim" style="width:30%;" onclick="location.href=\'' . get_bloginfo('wpurl') . '/wp-admin/admin.php?page=payment_gateways&activate_megasoft=true\';" class="green_alert pointer"><strong>' . __('Activate Megasoft Gateway?', 'event_espresso') . '</strong></li>';
+					}
+					echo '</ul>';
+					?>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php
+}
+
+//Authorize.net Settings Form
+function event_espresso_display_megasoft_settings() {
+	$megasoft_settings = get_option('event_espresso_megasoft_settings');
+	?>
+	<form method="post" action="<?php echo $_SERVER['REQUEST_URI'] ?>">
+		<table width="99%" border="0" cellspacing="5" cellpadding="5">
+			<tr>
+				<td valign="top"><ul>
+						<li>
+							<label for="megasoft_login_id">
+								<?php _e('Affiliation ID', 'event_espresso'); ?>
+							</label>
+							<input type="text" name="megasoft_login_id" size="35" value="<?php echo $megasoft_settings['megasoft_login_id']; ?>">
+						</li>
+					</ul></td>
+				<td valign="top">
+					<ul>
+						<li>
+							<label for="use_sandbox">
+								<?php _e('Account Uses Megasoft\'s development Server', 'event_espresso'); ?> <a class="thickbox" href="#TB_inline?height=300&width=400&inlineId=megasoft_sandbox"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>/images/question-frame.png" width="16" height="16" /></a>
+							</label>
+							<input id="sandbox_checkbox_aim" name="use_sandbox" type="checkbox" value="1" <?php echo $megasoft_settings['use_sandbox'] ? 'checked="checked"' : '' ?> />
+							 </li>
+							<li>
+							<label for="force_ssl_return">
+								<?php _e('Force HTTPS on Return URL', 'event_espresso'); ?>
+								<a class="thickbox" href="#TB_inline?height=300&width=400&inlineId=force_ssl_return"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>/images/question-frame.png" width="16" height="16" /></a>
+							</label>
+							<input name="force_ssl_return" type="checkbox" value="1" <?php echo $megasoft_settings['force_ssl_return'] ? 'checked="checked"' : '' ?> /></li>
+							<li>
+							<label for="display_header">
+								<?php _e('Display a Form Header', 'event_espresso'); ?>
+								<a class="thickbox" href="#TB_inline?height=300&width=400&inlineId=display_header"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>/images/question-frame.png" width="16" height="16" /></a>
+							</label>
+							<input name="display_header" type="checkbox" value="1" <?php echo $megasoft_settings['display_header'] ? 'checked="checked"' : '' ?> /></li>
+							<li>
+							<label for="header">
+								<?php _e('Header Text', 'event_espresso'); ?>
+							</label>
+							<br />
+							<input type="text" name="header" size="35" value="<?php echo $megasoft_settings['header']; ?>">
+						</li>
+					</ul></td>
+			</tr>
+		</table>
+		<p>
+			<input type="hidden" name="update_megasoft" value="update_megasoft">
+			<input class="button-primary" type="submit" name="Submit" value="<?php _e('Update Megasoft Settings', 'event_espresso') ?>" id="save_megasoft_settings" />
+		</p>
+	</form>
+	<?php
+}
+
+add_action('action_hook_espresso_display_gateway_settings','event_espresso_megasoft_payment_settings');
