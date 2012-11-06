46dfa0eb4b41a6222c65dd5a5ac4acb31cadee61
Brent Christenson <brent@eventespresso.com>
2012-11-06 18:03:58 +0000

    makes Add New Attendee admin page look MOAR BETTERER !!!  [touch:1603]

--- includes/admin-reports/add_new_attendee.php
+++ includes/admin-reports/add_new_attendee.php
@@ -76,25 +76,26 @@ function add_new_attendee($event_id){
 	jQuery(document).ready(function($jaer) {
 	jQuery(function(){
 		//Registration form validation
-		jQuery('#registration_form').validate();
+		jQuery('#espresso-admin-add-new-attendee-frm').validate();
 	});
 });
 
 	</script>
 <div class="metabox-holder">
   <div class="postbox">
-    <div id="event_espressotration_form">
+    <div id="espresso-admin-add-new-attendee-dv">
 
-        <form method="post" action="<?php echo $_SERVER['REQUEST_URI']?>" onsubmit="return validateForm(this)"  id="registration_form" class="espresso_form">
+        <form method="post" action="<?php echo $_SERVER['REQUEST_URI']?>" onsubmit="return validateForm(this)"  id="espresso-admin-add-new-attendee-frm" class="espresso_form">
 			<?php wp_nonce_field('reg_nonce', 'reg_form_nonce');?>
           <h3 class="h3_event_title" id="h3_event_title-<?php echo $event_id;?>"><?php echo $event_name?></h3>
            <div  class="padding">
      	     <div  class="inside">
+				<fieldset>
 		 		<h4 class="reg-quest-title section-title"><?php _e('Event Dates and Times','event_espresso'); ?></h4>
-		          <p class="start_date">
-					<span class="span_event_date_label"><?php _e('Start Date:','event_espresso'); ?></span><span class="span_event_date_value"><?php echo event_date_display($start_date)?></span>
+					<p class="start_date">
+						<span class="span_event_date_label"><?php _e('Start Date:','event_espresso'); ?></span><span class="span_event_date_value"><?php echo event_date_display($start_date)?></span>
 					</p>
-		          <p class="event_time">
+		          	<p class="event_time">
 		            <?php
 						$time_selected ='';
 						//This block of code is used to display the times of an event in either a dropdown or text format.
@@ -103,24 +104,21 @@ function add_new_attendee($event_id){
 						}else if ($time_selected == false){
 							echo event_espresso_time_dropdown($event_id);
 						}//End time selected
-	?>
-	          </p>
-	          <?php
-						/*
-						 * Added for seating chart addon
-						 */
+					?>
+	          		</p>
+	          		<?php
+						// Added for seating chart addon
 						do_action('ee_seating_chart_css');
 						do_action('ee_seating_chart_js');
 						do_action('ee_seating_chart_flush_expired_seats');
 						do_action( 'espresso_seating_chart_select', $event_id);
-						/*
-						 * End
-						 */
-
-			  ?>
+			  		?>
+				</fieldset>
 			  <?php
 						echo event_espresso_add_question_groups( $question_groups, '', null, 0, array('admin_only'=>true), 'inline' );
 
+						echo event_espresso_additional_attendees( $event_id, $additional_limit, $number_available_spaces, __('Number of Tickets', 'event_espresso'), true, 'admin', 'inline' );
+						
 						//Coupons
 						if (function_exists('event_espresso_coupon_registration_page')) {
 							echo event_espresso_coupon_registration_page($use_coupon_code, $event_id);
@@ -133,6 +131,8 @@ function add_new_attendee($event_id){
 
 
 	?>
+
+
 	          <p class="event_form_field">
 	            <label for="event_cost" class="inline">
 	              <?php _e('Amount Paid:','event_espresso'); ?>
@@ -145,7 +145,6 @@ function add_new_attendee($event_id){
 	          <p class="event_form_submit" id="event_form_submit-<?php echo $event_id;?>">
 	            <input class="event_form_submit button-primary" id="event_form_field-<?php echo $event_id;?>" type="submit" name="Submit" value="<?php _e('Submit','event_espresso');?>" />
 	          </p>
-			 	<?php echo event_espresso_additional_attendees( $event_id, $additional_limit, $number_available_spaces, __('Number of Tickets', 'event_espresso'), true, 'admin', 'inline' ); ?>
 	      </div>
 	      </div>
         </form>
