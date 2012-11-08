f5a588790f29bfa336354e8bb9baf2e2badfa369
Brent Christenson <brent@eventespresso.com>
2012-11-08 21:49:46 +0000

    reverts update query to what was in repo prior to bug, increments EE version to 3.1.28.2 [touch:1649]

--- espresso.php
+++ espresso.php
@@ -6,7 +6,7 @@
 
   Reporting features provide a list of events, list of attendees, and excel export.
 
-  Version: 3.1.28.1.P
+  Version: 3.1.28.2.P
 
   Author: Event Espresso
   Author URI: http://www.eventespresso.com
