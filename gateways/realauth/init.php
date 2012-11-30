cbb2aa337147c4db042ebc1b241aa3951b117698
Michael Nelson <michael@eventespresso.com>
2012-11-30 21:08:32 +0000

    Creating branch for google checkout

--- gateways/realauth/init.php
+++ gateways/realauth/init.php
@@ -9,4 +9,5 @@ if (!empty($_REQUEST['type']) && $_REQUEST['type'] == 'realauth') {
 	event_espresso_require_gateway("realauth/realauthprocesspayment.php");
 	add_filter('filter_hook_espresso_transactions_get_attendee_id', 'espresso_transactions_realauth_get_attendee_id');
 	add_filter('filter_hook_espresso_transactions_get_payment_data', 'espresso_process_realauth');
+	add_filter('filter_hook_espresso_thank_you_get_payment_data', 'espresso_process_realauth');
 }
