04f175d5ecb2036a7338dd58926d1a6dee68b27f
Darren Ethier <darren@roughsmootheng.in>
2013-01-03 14:04:33 -0500

    synchronizing versions (keep in mind that on release the version will be 3.1.30P)

--- espresso.php
+++ espresso.php
@@ -32,7 +32,7 @@
 //Define the version of the plugin
 function espresso_version() {
 	do_action('action_hook_espresso_log', __FILE__, __FUNCTION__, '');
-	return '3.1.30.P-beta';
+	return '3.1.30.2P-beta';
 }
 
 //This tells the system to check for updates to the paid version
