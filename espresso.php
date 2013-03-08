53c7197ff3a35796386ddadd048ffa932fae491a
Michael Nelson <michael@eventespresso.com>
2013-03-08 10:23:01 -0800

    tweaked espresso.php to see if line endings will get updated

--- espresso.php
+++ espresso.php
@@ -29,7 +29,7 @@
  */
 
 
-//Define the version of the Plugin
+//Define the version of the Plugin 
 function espresso_version() {
 	do_action('action_hook_espresso_log', __FILE__, __FUNCTION__, '');
 	return '3.1.30.7P';
