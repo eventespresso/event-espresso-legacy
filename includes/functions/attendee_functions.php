a257fc545fde3285c0a7bf8ad38d3f432eecfc2f
Brent Christensen <brent@eventespresso.com>
2013-03-19 11:48:54 -0700

    fixes calls to removed functions [touch:2340]

--- includes/functions/attendee_functions.php
+++ includes/functions/attendee_functions.php
@@ -62,7 +62,6 @@ function add_attendee_questions($questions, $registration_id, $attendee_id = 0, 
 							
 							if ($question->admin_only != 'Y') {
 								$post_val = ( $question->system_name != '' ) ? $response_source[$question->system_name] : $question_type;
-								$post_val = espresso_parse_question_response_for_price( trim( $post_val ), $question->price_mod, $attendee_id );
 							} else {
 								$post_val = '';
 							}
@@ -74,7 +73,6 @@ function add_attendee_questions($questions, $registration_id, $attendee_id = 0, 
 							if ( ! empty( $response_source[$question->question_type . '_' . $question->qstn_id] ) && $question->admin_only != 'Y' ) {
 								for ( $i = 0; $i < count( $response_source[$question->question_type . '_' . $question->qstn_id] ); $i++ ) {
 									$val = trim( $response_source[$question->question_type . '_' . $question->qstn_id][$i] );
-									$val = espresso_parse_question_response_for_price( $val, $question->price_mod, $attendee_id );
 									$post_val .= $val . ",";
 								}
 							}
