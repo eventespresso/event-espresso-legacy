a9792fedb875c46b42256ed8ed31550eb4e2144b
Sidney Harrell <sidney@eventespresso.com>
2013-04-07 21:13:05 -0400

    fixed headers for ical [touch:2471]

--- includes/functions/ical.php
+++ includes/functions/ical.php
@@ -27,19 +27,15 @@ function espresso_ical() {
 		if (ob_get_length()){
 		echo('Some data has already been output, can\'t send iCal file');
 		}
-	header('Content-Type: application/x-download');
+	header('Content-Type: text/calendar; charset=utf-8');
 		if (headers_sent()){
 		echo('Some data has already been output, can\'t send iCal file');
 		}
 	header('Content-Length: ' . strlen($output));
-	header('Content-Disposition: attachment; filename="' . $name . '"');
+	header('Content-Disposition: inline; filename="' . $name . '"');
 	header('Cache-Control: private, max-age=0, must-revalidate');
 	header('Pragma: public');
-	header('Content-Type: application/octet-stream');
-	header('Content-Type: application/force-download');
-	header('Content-type: application/pdf');
 	header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
-	header("Content-Transfer-Encoding: binary");
 	header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 	ini_set('zlib.output_compression', '0');
 	echo $output;
