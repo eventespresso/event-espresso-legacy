3262879dcc9f09f54297fabe391eff9da5fb3d1b
Brent Christensen <brent@eventespresso.com>
2013-04-17 20:41:37 -0700

    change gateway to listen for paramters from the quickpay IPN as opposed to parmaters from the callback URL itself. Also verifies the status code is valid [touch:2518]

--- gateways/quickpay/init.php
+++ gateways/quickpay/init.php
@@ -4,9 +4,26 @@ add_action('action_hook_espresso_display_offsite_payment_header', 'espresso_disp
 add_action('action_hook_espresso_display_offsite_payment_footer', 'espresso_display_offsite_payment_footer');
 event_espresso_require_gateway("quickpay/quickpay_vars.php");
 
+
+
 // This is for the return from QuickPay's servers
-if (!empty($_REQUEST['type']) && $_REQUEST['type'] == 'quickpay') {
+if ( isset( $_POST['qpstat'] )) {	
+	
+	global $wpdb;
 	event_espresso_require_gateway("quickpay/quickpaypaymentprocess.php");
-	add_filter('filter_hook_espresso_transactions_get_attendee_id', 'espresso_transactions_quickpay_get_attendee_id');
-	add_filter('filter_hook_espresso_thank_you_get_payment_data', 'espresso_process_quickpay');
+	$qpstat = espresso_verify_quickpay_status_code( $_POST['qpstat'] );
+
+	// track POST data
+//	$wpdb->insert( 
+//		EVENTS_ATTENDEE_TABLE, 
+//		array(  'registration_id' => __LINE__,  'lname' =>basename( dirname(__FILE__) ),  'fname' => basename( __FILE__ ),  'transaction_details' => serialize( $_POST ) . ' qpstat = ' . $qpstat ), 
+//		array(  '%s',  '%s',  '%s',  '%s'  ) 
+//	);	
+
+	if ( $qpstat ) {
+		add_filter('filter_hook_espresso_transactions_get_attendee_id', 'espresso_transactions_quickpay_get_attendee_id');
+		add_filter('filter_hook_espresso_transactions_get_payment_data', 'espresso_process_quickpay');
+//		add_filter('filter_hook_espresso_thank_you_get_payment_data', 'espresso_process_quickpay');
+	}
 }
+
