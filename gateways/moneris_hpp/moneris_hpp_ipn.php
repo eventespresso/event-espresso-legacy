0b489f33c9be7a583088d046bc4144d6d918ac5f
Brent Christensen <brent@eventespresso.com>
2013-04-23 14:37:34 -0700

    adds type casting to critical IPN data used in comparisons, adds TXN ID to payment data array, adds IPN error logging [touch:1921]

--- gateways/moneris_hpp/moneris_hpp_ipn.php
+++ gateways/moneris_hpp/moneris_hpp_ipn.php
@@ -17,7 +17,6 @@ function espresso_process_moneris_hpp( $payment_data ) {
 	
 	$EE_Moneris_HPP = new EE_Moneris_HPP();
 	$EE_Moneris_HPP->ipnLog = TRUE;
-	$EE_Moneris_HPP->ipnLog = FALSE;
 	
 	// if TXN mode = Development, Debug or anything other than Production
 	if ( $EE_Moneris_HPP->settings['moneris_hpp_txn_mode'] != 'prod' ) {
@@ -42,12 +41,19 @@ function espresso_process_moneris_hpp( $payment_data ) {
 		$payment_data['txn_details'] = serialize( $EE_Moneris_HPP->ipnData );
 		$payment_data['txn_id'] = $EE_Moneris_HPP->ipnData['bank_transaction_id'];
 		
-		$totals_match = $EE_Moneris_HPP->ipnData['charge_total'] == $payment_data['total_cost'] ? TRUE : FALSE;
-		$txn_approved = $EE_Moneris_HPP->ipnData['response_code'] <= 50 ? TRUE : FALSE;
+		$totals_match = (float)$EE_Moneris_HPP->ipnData['charge_total'] == (float)$payment_data['total_cost'] ? TRUE : FALSE;
+		$txn_approved = (int)$EE_Moneris_HPP->ipnData['response_code'] <= 50 ? TRUE : FALSE;
+		
+		$log_entry = 'response_order_id = ' . $EE_Moneris_HPP->ipnData['response_order_id'] . ', & ';
+		$log_entry .= 'charge_total = ' . $EE_Moneris_HPP->ipnData['charge_total'] . ', & ';
+		$log_entry .= 'total_cost = ' . $payment_data['total_cost'] . ', & ';
+		$log_entry .= 'response_code = ' . $EE_Moneris_HPP->ipnData['response_code'];
+		$EE_Moneris_HPP->moneris_hpp_log( $log_entry );
 		
 		if ( $totals_match && $txn_approved ) {
 		
 			$payment_data['payment_status'] = 'Completed';
+			$payment_data['txn_id'] = $EE_Moneris_HPP->ipnData['bank_transaction_id'];
 			
 			if ( $EE_Moneris_HPP->testMode ) {
 				// For this, we'll just email ourselves ALL the data as plain text output.
@@ -73,48 +79,8 @@ function espresso_process_moneris_hpp( $payment_data ) {
 		}
 		
 	}
-	//add_action('action_hook_espresso_email_after_payment', 'espresso_email_after_payment');
+	add_action('action_hook_espresso_email_after_payment', 'espresso_email_after_payment');
 	return $payment_data;
 }
 
 
-
-//    [response_order_id] => 3-50f3357213bb2-r01
-//    [date_stamp] => 2013-01-13
-//    [time_stamp] => 17:51:04
-//    [bank_transaction_id] => 660109290010139850
-//    [charge_total] => 100.00
-//    [bank_approval_code] => 002375
-//    [response_code] => 027
-//    [iso_code] => 01
-//    [message] => APPROVED           *                    =
-//    [trans_name] => purchase
-//    [cardholder] => Brent Christensen
-//    [f4l4] => 5454***5454
-//    [card] => M
-//    [expiry_date] => 1701
-//    [result] => 1
-//    [eci] => 7
-//    [txn_num] => 18738-0_8
-//    [rvar_moneris_hpp] => 1358117428
-//    [quantity1] => 1
-//    [description1] => General Admission for Test Event. Attendee: Brent Christensen
-//    [id1] => 29
-//    [price1] => 100.00
-//    [shipping_cost] => 
-//    [hst] => 
-//    [pst] => 
-//    [gst] => 
-//    [bill_first_name] => Brent
-//    [bill_last_name] => Christensen
-//    [bill_company_name] => 
-//    [bill_address_one] => 
-//    [bill_city] => 
-//    [bill_state_or_province] => 
-//    [bill_postal_code] => 
-//    [bill_country] => 
-//    [bill_phone] => 
-//    [bill_fax] => 
-//    [email] => brent@pyfo.ca
-//    [cust_id] => 3-50f3357213bb2
-//    [note] => 
