4578e271f496e8d4a25e80ca26bf533fe5fd88db
Seth <seth@eventespresso.com>
2013-04-22 19:00:16 -0600

    Removing old code

--- gateways/infusionsoft_payment/init.php
+++ gateways/infusionsoft_payment/init.php
@@ -6,7 +6,6 @@ event_espresso_require_gateway("infusionsoft_payment/infusionsoft_vars.php");
 
 // This is for the thank you page to process the transaction
 if (!empty($_REQUEST['infusionsoft_payment'])) {
-	//event_espresso_require_gateway("infusionsoft_payment/infusionsoft_ipn.php");
 	add_filter('filter_hook_espresso_transactions_get_attendee_id', 'espresso_transactions_infusionsoft_get_attendee_id');
 	add_filter('filter_hook_espresso_thank_you_get_payment_data', 'espresso_process_infusionsoft_payment');
 }
