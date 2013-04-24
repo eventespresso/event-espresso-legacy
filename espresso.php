4c1a1d82572d35ac90beda6fbe982cc4dc09bd80
Seth <seth@eventespresso.com>
2013-04-24 11:23:28 -0600

    Updating version and copyright date.

--- espresso.php
+++ espresso.php
@@ -6,12 +6,12 @@
 
   Reporting features provide a list of events, list of attendees, and excel export.
 
-  Version: 3.1.31.1
+  Version: 3.1.31.2
 
   Author: Event Espresso
   Author URI: http://www.eventespresso.com
 
-  Copyright (c) 2008-2012 Event Espresso  All Rights Reserved.
+  Copyright (c) 2008-2013 Event Espresso  All Rights Reserved.
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -32,7 +32,7 @@
 //Define the version of the plugin
 function espresso_version() {
 	do_action('action_hook_espresso_log', __FILE__, __FUNCTION__, '');
-	return '3.1.31.1';
+	return '3.1.31.2';
 }
 
 //This tells the system to check for updates to the paid version
