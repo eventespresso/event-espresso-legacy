24be43c6990d2c83067e048fcf2263b20fd9d668
Seth <seth@eventespresso.com>
2013-05-30 01:40:52 -0600

    Adding a few actions to the left and right columns of the event editors. Cleaning up a little bit of code too. [touch:2834]

--- includes/event-management/edit_event.php
+++ includes/event-management/edit_event.php
@@ -203,6 +203,7 @@ function edit_event($event_id = 0) {
 	<!-- /submitdiv -->
 
 	<?php
+	do_action('action_hook_espresso_edit_event_right_column_top', $event_id);
 	$advanced_options = '';
 	if (file_exists(EVENT_ESPRESSO_PLUGINFULLPATH . 'includes/admin-files/event-management/advanced_settings.php')) {
 		require_once(EVENT_ESPRESSO_PLUGINFULLPATH . "includes/admin-files/event-management/advanced_settings.php");
@@ -394,10 +395,11 @@ function edit_event($event_id = 0) {
 		</div>
 		<!-- /groupon-options -->
 			<?php
-			}
-			$sidebar_content = ob_get_clean();
-			ob_start();
-			?>
+	}
+	do_action('action_hook_espresso_edit_event_right_column_bottom', $event_id);
+	$sidebar_content = ob_get_clean();
+	ob_start();
+	?>
 	<div id="titlediv"> <strong>
 	<?php _e('Event Title', 'event_espresso'); ?>
 		</strong>
@@ -560,6 +562,7 @@ function edit_event($event_id = 0) {
 		<h2>
 	<?php _e('Advanced Options', 'event_espresso'); ?>
 		</h2>
+		<?php do_action('action_hook_espresso_edit_event_left_column_advanced_options_top', $event_id);?>
 		<div id="event-location" class="postbox">
 			<div class="handlediv" title="Click to toggle"><br />
 			</div>
