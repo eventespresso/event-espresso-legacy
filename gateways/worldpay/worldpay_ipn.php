28a030405032303306b7ab5d8edc7e21959deb47
Michael Nelson <michael@eventespresso.com>
2013-06-27 15:41:10 -0700

    [touch:2950]{t:15}removed sidneys fix in order to confirm this general fix works instead

--- gateways/worldpay/worldpay_ipn.php
+++ gateways/worldpay/worldpay_ipn.php
@@ -19,9 +19,10 @@ function espresso_process_worldpay($payment_data) {
 	$payment_data['payment_status'] = "Incomplete";
 	$payment_data['txn_id'] = 0;
 	$payment_data['txn_details'] = serialize($_REQUEST);
-	session_id($_REQUEST['MC_session_id']);
-	session_start();
-	session_destroy();
+	//removed sidney's fix, as hopefully we'll have a more general fix taht will work for all gateayss
+//	session_id($_REQUEST['MC_session_id']);
+//	session_start();
+//	session_destroy();
 	if ($_REQUEST['transStatus'] == 'Y') {
 		$attendee_id = $payment_data['attendee_id'];
 		$payment_data['payment_status'] = 'Completed';
