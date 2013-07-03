023a9a0cf3cb3f615f0bbf1fe8ada66d5e70d6d3
Brent Christensen <brent@eventespresso.com>
2013-07-03 13:36:26 -0700

    hide payment page when "bypass payment page" option is in affect for off-site payment gateways, and allow leave page  [touch:3035]

--- scripts/espresso_payment_page.js
+++ scripts/espresso_payment_page.js
@@ -1,10 +1,20 @@
 jQuery(document).ready(function($) {
 
+	var preventLeavePage = true;
+	var bypass_payment_page = $('#bypass_payment_page').val();
+	
+	if( bypass_payment_page == 'true' ) {
+		var bpp = $('#bypass_payment_page-dv').html();
+		$('#espresso-payment_page-dv').html( bpp );
+		$('.payment_option_title').hide();
+		$('.payment-option-dv').hide();
+		$('.payment-option-dv a').hide();
+		$('#bypass_payment_page_gateway_form').submit();
+	}
+	
 	$('.hide-if-js').hide();
 	$('.payment_container').toggleClass('payment-option-closed'); 
 	$('.payment-option-dv').toggleClass('payment-option-closed'); 
-	
-	var preventLeavePage = true;
 	
 	// generic click event for displaying and giving focus to an element and hiding control 
 	$('.display-the-hidden').on( 'click', function() {
@@ -43,7 +53,7 @@ jQuery(document).ready(function($) {
 	});
 	
 	window.onbeforeunload = function() {
-		if ( preventLeavePage ) {
+		if ( preventLeavePage && bypass_payment_page != 'true' ) {
 	  	  return 'Warning!!! Using the back button will overwrite your existing registration.';
 		}
 	}	
