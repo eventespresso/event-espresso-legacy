43f92af4f92acc55c5cf3d53aafac55cd2f77d72
Seth <seth@eventespresso.com>
2013-08-21 11:38:45 -0600

    Adding Brent's changes to adjust prices based on the PM add-on. These changes don't seem to affect standard registrations or discounts.
    Also updated the confrimation_display template to show singular or plural text based on the amount of attendees. [touch:3459]

--- templates/confirmation_display.php
+++ templates/confirmation_display.php
@@ -6,6 +6,7 @@ do_action('action_hook_espresso_log', __FILE__, 'FILE LOADED', '');
 //This page gets all of the varaibles from includes/process-registration/payment_page.php
 //Payment confirmation block
 $attendee_num = apply_filters('action_hook_espresso_confirmation_page_primary_attendee_count',1);
+
 ?>
 	<form id="form1" name="form1" method="post" action="<?php echo get_permalink($org_options['event_page_id']);?>">
 		<div class="event-conf-block event-display-boxes ui-widget" >
@@ -35,11 +36,22 @@ $attendee_num = apply_filters('action_hook_espresso_confirmation_page_primary_at
 				if ( $display_price ) {
 ?>	
 					<tr>
-						<th scope="row" class="header">
-							<?php echo empty($price_type) ? __('Price per attendee:','event_espresso') : __('Type/Price per attendee:','event_espresso'); ?>
+						<th scope="row" class="header" valign="top">
+							<?php echo sprintf( _n( 'Attendee Price', 'Attendee Prices', $total_attendees, 'event_espresso' ), $total_attendees ); ?>
 						</th>
 						<td>
-							<span class="event_espresso_value"><?php echo empty($price_type) ? $org_options['currency_symbol'] . number_format($final_price,2) : stripslashes_deep($price_type) . ' / ' .$org_options['currency_symbol'].number_format($final_price,2);?></span>
+							<span class="event_espresso_value">
+								<?php 
+									if ( ! empty( $attendee_prices )) {
+										foreach ( $attendee_prices as $nmbr => $attendee_price ) {
+											// turn generic array key into counter
+											$nmbr++;
+											echo ($total_attendees > 1 ? $nmbr . ') ' : '') . $attendee_price['option'] . ' ' . $org_options['currency_symbol'] . number_format( $attendee_price['price'], 2 );
+											echo $attendee_price['qty'] > 1 ? ' x ' . $attendee_price['qty'] . '<br/>' : '<br/>';
+										}
+									}
+								?>								
+							</span>
 						</td>
 					</tr>
 <?php
@@ -63,12 +75,12 @@ $attendee_num = apply_filters('action_hook_espresso_confirmation_page_primary_at
 ?>
 					<tr>
 					<th scope="row" class="header">
-						<?php _e('Attendee Name:','event_espresso'); ?>
+						<?php echo sprintf( _n( 'Attendee Name', 'Attendee Names', $total_attendees, 'event_espresso' ), $total_attendees ); ?>
 					</th>
 					<td  valign="top">
 						<span class="event_espresso_value"><?php echo stripslashes_deep($attendee_name)?> (<?php echo $attendee_email?>) 
 <?php 
-						echo '<a href="'.home_url().'/?page_id='.$event_page_id.'&amp;registration_id='.$registration_id.'&amp;id='.$attendee_id.'&amp;regevent_action=edit_attendee&amp;primary='.$attendee_id.'&amp;event_id='.$event_id.'&amp;attendee_num='.$attendee_num.'">'. __('Edit', 'event_espresso').'</a>';  // removed p_id='.$p_id.'&amp; coupon_code='.$coupon_code.'&amp;groupon_code='.$groupon_code.'&amp;
+						echo '<a href="'.home_url().'/?page_id='.$event_page_id.'&amp;registration_id='.$registration_id.'&amp;id='.$attendee_id.'&amp;regevent_action=edit_attendee&amp;primary='.$attendee_id.'&amp;event_id='.$event_id.'&amp;attendee_num='.$attendee_num.'">'. __('Edit', 'event_espresso').'</a>';
 						
 						//Create additional attendees
 						$sql = "SELECT * FROM " . EVENTS_ATTENDEE_TABLE;
@@ -87,9 +99,9 @@ $attendee_num = apply_filters('action_hook_espresso_confirmation_page_primary_at
 									echo "(" . $x_attendee['email']  . ") ";
 								}
 								//Create edit link
-								echo '<a href="'.home_url().'/?page_id='.$event_page_id.'&amp;registration_id='.$registration_id.'&amp;id='.$x_attendee['id'].'&amp;regevent_action=register&amp;form_action=edit_attendee&amp;primary='.$attendee_id.'&amp;p_id='.$attendee_id.'&amp;attendee_num='.$attendee_num.'&amp;event_id='.$event_id.'">'. __('Edit', 'event_espresso').'</a>'; // removed coupon_code='.$coupon_code.'&amp;groupon_code='.$groupon_code.'&amp;
+								echo '<a href="'.home_url().'/?page_id='.$event_page_id.'&amp;registration_id='.$registration_id.'&amp;id='.$x_attendee['id'].'&amp;regevent_action=register&amp;form_action=edit_attendee&amp;primary='.$attendee_id.'&amp;p_id='.$attendee_id.'&amp;attendee_num='.$attendee_num.'&amp;event_id='.$event_id.'">'. __('Edit', 'event_espresso').'</a>';
 								//Create delete link
-								echo ' | <a href="'.home_url().'/?page_id='.$event_page_id.'&amp;registration_id='.$registration_id.'&amp;id='.$x_attendee['id'].'&amp;regevent_action=register&amp;form_action=edit_attendee&amp;primary='.$attendee_id.'&amp;delete_attendee=true&amp;p_id='.$attendee_id.'&amp;event_id='.$event_id.'">'. __('Delete', 'event_espresso').'</a>'; // removed coupon_code='.$coupon_code.'&amp;groupon_code='.$groupon_code.'&amp;
+								echo ' | <a href="'.home_url().'/?page_id='.$event_page_id.'&amp;registration_id='.$registration_id.'&amp;id='.$x_attendee['id'].'&amp;regevent_action=register&amp;form_action=edit_attendee&amp;primary='.$attendee_id.'&amp;delete_attendee=true&amp;p_id='.$attendee_id.'&amp;event_id='.$event_id.'">'. __('Delete', 'event_espresso').'</a>';
 							}
 						}
 ?>
@@ -111,20 +123,21 @@ $attendee_num = apply_filters('action_hook_espresso_confirmation_page_primary_at
 						<?php _e('Total Price:','event_espresso'); ?>
 					</th>
 					<td>
-						<span class="event_espresso_value"><?php echo $display_cost;  //echo $event_discount_label;?></span>
+						<span class="event_espresso_value"><?php echo $display_cost; ?></span>
 					</td>
 				</tr>
 			</table>
+</div>
 			<p class="espresso_confirm_registration">
 			<input class="btn_event_form_submit ui-button ui-button-big ui-priority-primary ui-state-default ui-state-hover ui-state-focus ui-corner-all" type="submit" name="confirm" id="confirm" value="<?php _e('Confirm Registration', 'event_espresso'); ?>&nbsp;&raquo;" />
 		    </p>
-		</div>
+		
 		
 		<?php if ($display_questions != '') { ?>
 		
-		<div class="additional-conf-info event-display-boxes">
+		<div  id="additional-conf-info" class="additional-conf-info event-display-boxes">
 				<h3 class="event_title ui-widget-header ui-corner-top"><?php echo stripslashes_deep($attendee_name)?></h3>
-				<div class="additional-conf-info-inner event-data-display ui-widget-content ui-corner-bottom">
+				<div id="additional-conf-info" class="additional-conf-info-inner event-data-display ui-widget-content ui-corner-bottom">
 					<table id="event_espresso_attendee_verify_questions" class="event-display-tables grid">
 					<?php foreach ($questions as $question) { ?>
 						<tr>
@@ -136,14 +149,15 @@ $attendee_num = apply_filters('action_hook_espresso_confirmation_page_primary_at
 							</td>
 						</tr>
 					<?php } ?>
-					</table>
-					<p class="espresso_confirm_registration">
-						<input class="btn_event_form_submit ui-button ui-button-big ui-priority-primary ui-state-default ui-state-hover ui-state-focus ui-corner-all" type="submit" name="confirm2" id="confirm2" value="<?php _e('Confirm Registration', 'event_espresso'); ?>&nbsp;&raquo;" />
-					</p>             
+					</table>              
 				</div>
 				<!-- / .event-data-display -->   
 			</div>
 			<!-- / .event-display-boxes -->   
+			
+			<p class="espresso_confirm_registration">
+				<input class="btn_event_form_submit ui-priority-primary ui-widget-content ui-corner-all" type="submit" name="confirm2" id="confirm2" value="<?php _e('Confirm Registration', 'event_espresso'); ?>&nbsp;&raquo;" />
+			</p>
 			
 		<?php	} ?>
 								
