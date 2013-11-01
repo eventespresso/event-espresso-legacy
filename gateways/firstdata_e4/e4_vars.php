81145c4db49bc36d20f60d4a6f106550de68cd41
Seth <seth@eventespresso.com>
2013-11-01 12:14:53 -0600

    Fixes FirstData E4. [touch:3659]

--- gateways/firstdata_e4/e4_vars.php
+++ gateways/firstdata_e4/e4_vars.php
@@ -31,12 +31,8 @@ function espresso_display_firstdata_e4($payment_data) {
 	$myE4->addField('x_show_form', 'PAYMENT_FORM');
 	$myE4->addField('x_reference_3', $registration_id . ' FDe4');
 	$myE4->addField('x_relay_response', 'TRUE');
-	if ($firstdata_e4_settings['force_ssl_return']) {
-		$home = str_replace("http://", "https://", home_url());
-	} else {
-		$home = home_url();
-	}
-	$myE4->addField('x_relay_url', $home . '/?type=firstdata_e4&page_id=' . $org_options['return_url']);
+	
+	$myE4->addField('x_relay_url', espresso_build_gateway_url('return_url', $payment_data, 'firstdata_e4'));
 	$myE4->addField('x_description', stripslashes_deep($event_name) . ' ' . __('Reg. ID:', 'event_espresso') . ' ' . $attendee_id . ' ' . __('Name:', 'event_espresso') . ' ' . stripslashes_deep($fname . ' ' . $lname) . ' ' . __('Total Registrants:', 'event_espresso') . ' ' . $quantity);
 	$myE4->addField('x_logo_url', $image_url);
 	//$myE4->addField('x_invoice_num', event_espresso_session_id());
