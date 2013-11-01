97431ae5c3e84f556ded78e632f68d45a469bdfb
Seth <seth@eventespresso.com>
2013-11-01 09:58:33 -0600

    Fixes sway rapid 3. [touch:3659]

--- gateways/eway_rapid3/eway_rapid3_vars.php
+++ gateways/eway_rapid3/eway_rapid3_vars.php
@@ -41,7 +41,7 @@ function espresso_display_eway_rapid3($data) {
 				'CurrencyCode'=>$eway_rapid3_settings['currency_format']
 		);
 		$eway_rapid3RequestData = array('Payment'=>$payment);
-		$redirectUrl=site_url().'?page_id='.$org_options['return_url'].'&r_id=' . $payment_data['registration_id'].'&eway_rapid3=true&id='.$payment_data['attendee_id']; 
+		$redirectUrl = add_query_arg(array('r_id'=>$registration_id,'eway_rapid3'=>'true','id'=>$attendee_id), get_permalink($org_options['return_url']));
 		$rapid3Response= $rapid3Client->createAccessCode($eway_rapid3RequestData,$redirectUrl,'ProcessPayment');
 		if( empty($rapid3Response)){
 			echo '<div id="message" class="clear"><p class="error">**' . __( 'An error occcurred communicating with EWay Rapid 3 Gateway\'s Server. You probably have the wrong API Key', 'event_espresso' ) . '</p></div>';
