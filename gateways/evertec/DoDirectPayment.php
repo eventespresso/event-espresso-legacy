a906930854d27d76792c3ec6147c1fadee259e5f
Michael Nelson <michael@eventespresso.com>
2014-01-16 13:26:28 -0800

    [touch:3346]{t:45}cleaned up evertec gateway code

--- gateways/evertec/DoDirectPayment.php
+++ gateways/evertec/DoDirectPayment.php
@@ -9,24 +9,14 @@ function espresso_transactions_evertec_get_attendee_id($attendee_id) {
 }
 
 function espresso_process_evertec($payment_data) {
-// 	echo 'echodump of $payment_data';
-// 	var_dump($payment_data);
-// 	echo 'echodump of $_REQUEST';
-// 	var_dump($_REQUEST);
 	$r=$_REQUEST;
 	$evertec_settings = get_option('event_espresso_evertec_settings');
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-if($evertec_settings['use_sandbox']){
+	//prepare request
+	if($evertec_settings['use_sandbox']){
 		$server_url = 'https://everpaycert.evertecinc.com/cpsh2h/serviceh2h.asmx';//'https://everpaycert.evertecinc.com/wscheckoutpayment/wsCheckoutPayment.asmx?op=MakePayment';
 	}else{
 		$server_url = 'https://mmpay.evertecinc.com/cpsh2h/serviceh2h.asmx';  // 'https://mmpay.evertecinc.com/webservicev2/wscheckoutpayment.asmx';
 	}
-	
-
-	
-	
-	////////////////////////////////////////
 	$params = array(
 		'userName'=>$evertec_settings['username'],
 		'passWord'=>$evertec_settings['password'],
@@ -58,8 +48,6 @@ if($evertec_settings['use_sandbox']){
 		'Note'=>'',
 		'paymentType'=>$r['evertec_payment_method'],	
 			);
-	//was it ac redit card purchase?
-//	if(in_array($r['evertec_payment_method'],array('A','V','M','X'))){
 		$cardNumber = $r['card_num'];
 		$securityCardCode = $r['cvv'];
 		$expirationDate = $r['expmonth'].$r['expyear'];
@@ -67,7 +55,6 @@ if($evertec_settings['use_sandbox']){
 		$bankAccountNumber=$r['bankAccountNumber'];
 		$bankClientName=$r['bankClientName'];
 		$authorizationBit= isset($r['authorizationBit']) ? 1 : 0;
-//	}
 		$params = array_merge($params,array(
 			'cardNumber'=>$cardNumber,
 			'expirationDate'=>$expirationDate,
@@ -84,48 +71,27 @@ if($evertec_settings['use_sandbox']){
 		$raw_xml_body = '<?xml version="1.0" encoding="utf-8" ?><soap:Envelope	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
 			xmlns:xsd="http://www.w3.org/2001/XMLSchema"
 			xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" ><soap:Body><SendTransactions xmlns="http://tempuri.org/cpsh2h/serviceh2h">'.$xml_params.'</SendTransactions></soap:Body></soap:Envelope>';
-		
-//		echo htmlentities($raw_xml_body);
-//		echo $response['body'];die;
-//		$client = new SoapClient('https://mmpay.evertecinc.com/webservicev2/wscheckoutpayment.asmx?wsdl', array('trace'=>true,'soap_version' => SOAP_1_2));
-//			echo 'echodump of $client->__getFunctions()';
-//			var_dump($client->__getFunctions());
-	
-//			echo 'echodump of $client->__getTypes()';
-//			var_dump($client->__getTypes());
-//			try{
-//				$makePaymentResponse = $client->MakePayment(array(array('MakePayment'=>$params)));
-//				echo 'echodump of $makePaymentResponse';
-//				var_dump($makePaymentResponse);
-//			}catch(Exception $e){
-//				echo 'echodump of $e';
-//				var_dump($e);
-//			}
-	
- 			$soap_client_req = '<?xml version="1.0" encoding="UTF-8"?><env:Envelope xmlns:env="http://www.w3.org/2003/05/soap-envelope" xmlns:ns1="http://tempuri.org/WebMerchant/MerchantService"><env:Body><ns1:MakePayment/></env:Body></env:Envelope>';
-//	echo 'echodump of $client->__getLastRequest ()';
-//	var_dump($client->__getLastRequest ());
-//	echo 'echodump of $client->__getLastRequestHeaders();';
-//	var_dump($client->__getLastRequestHeaders());
 	
 // 	echo "raw xml:".htmlentities($raw_xml_body)."<hr>";		
-			$response = wp_remote_post($server_url,
-				array(
-					'headers'=>array(
-						'Content-Type'=>'text/xml; charset=utf-8;',
-						"User-Agent"=> "PHP-SOAP/5.4.4",
-						'Connection'=> 'Keep-Alive',
-						'SOAPAction'=> 'http://tempuri.org/cpsh2h/serviceh2h/SendTransactions'),//  'http://tempuri.org/WebMerchant/MerchantService/MakePayment'), 
-					'method'=>'POST',
-					'httpversion' => '1.1',
-					'body'=>$raw_xml_body,
-					'sslverify' => false));
-		echo 'echodump of $response';
-		echo htmlentities($response['body']);
+		//send request
+		$response = wp_remote_post($server_url,
+			array(
+				'headers'=>array(
+					'Content-Type'=>'text/xml; charset=utf-8;',
+					"User-Agent"=> "PHP-SOAP/5.4.4",
+					'Connection'=> 'Keep-Alive',
+					'SOAPAction'=> 'http://tempuri.org/cpsh2h/serviceh2h/SendTransactions'), 
+				'method'=>'POST',
+				'httpversion' => '1.1',
+				'body'=>$raw_xml_body,
+				'sslverify' => false));
+//		echo 'echodump of $response';
+//		echo htmlentities($response['body']);
 // 		die;
 		
+		//process response
 		$payment_data['payment_status'] = 'Incomplete';
-		$payment_data['txn_type'] = 'evertec';
+		$payment_data['txn_type'] = 'EverTec';
 		$payment_data['txn_id'] = 0;
 		$payment_data['txn_details'] = serialize($_REQUEST);
 		
@@ -144,7 +110,7 @@ if($evertec_settings['use_sandbox']){
 				$authorizationNumber = is_array($authorizationNumberElements) ? $authorizationNumberElements[0] : NULL;
 				if($paymentResult == '0000' && $confirmationNumber && $authorizationNumber){
 					$payment_data['payment_status'] = 'Completed';
-					$payment_data['txn_id'] = 'conf:'.$confirmationNumber.",auth:'".$authorizationNumber;
+					$payment_data['txn_id'] = 'conf:'.$confirmationNumber.",auth:".$authorizationNumber;
 					$error_message = false;
 				}else{
 					//they sent back an error message
@@ -162,67 +128,5 @@ if($evertec_settings['use_sandbox']){
 	if( $error_message ){
 		printf(__("An error occurred processing your payment: %s", "event_espresso"),$error_message);
 	}
-
-//	if ($use_sandbox) {
-//
-//		echo '<h3 style="color:#ff0000;" title="Payments will not be processed">' . __('Evertec Debug Mode Is Turned On', 'event_espresso') . '</h3>';
-//		$myEvertec->dump_fields();
-//	}
 	return $payment_data;
 }
-
-function APICallSuccessful($ack) {
-	if (strtoupper($ack) != 'SUCCESS' && strtoupper($ack) != 'SUCCESSWITHWARNING' && strtoupper($ack) != 'PARTIALSUCCESS')
-		return false;
-	else
-		return true;
-}
-
-function GetErrors($DataArray) {
-
-	$Errors = array();
-	$n = 0;
-	while (isset($DataArray['L_ERRORCODE' . $n . ''])) {
-		$LErrorCode = isset($DataArray['L_ERRORCODE' . $n . '']) ? $DataArray['L_ERRORCODE' . $n . ''] : '';
-		$LShortMessage = isset($DataArray['L_SHORTMESSAGE' . $n . '']) ? $DataArray['L_SHORTMESSAGE' . $n . ''] : '';
-		$LLongMessage = isset($DataArray['L_LONGMESSAGE' . $n . '']) ? $DataArray['L_LONGMESSAGE' . $n . ''] : '';
-		$LSeverityCode = isset($DataArray['L_SEVERITYCODE' . $n . '']) ? $DataArray['L_SEVERITYCODE' . $n . ''] : '';
-
-		$CurrentItem = array(
-				'L_ERRORCODE' => $LErrorCode,
-				'L_SHORTMESSAGE' => $LShortMessage,
-				'L_LONGMESSAGE' => $LLongMessage,
-				'L_SEVERITYCODE' => $LSeverityCode
-		);
-
-		array_push($Errors, $CurrentItem);
-		$n++;
-	}
-
-	return $Errors;
-}
-
-function DisplayErrors($Errors) {
-	echo '<p><strong class="credit_card_failure">Attention: Your transaction was declined for the following reason(s):</strong><br />';
-	foreach ($Errors as $ErrorVar => $ErrorVal) {
-		$CurrentError = $Errors[$ErrorVar];
-		foreach ($CurrentError as $CurrentErrorVar => $CurrentErrorVal) {
-			if ($CurrentErrorVar == 'L_ERRORCODE')
-				$CurrentVarName = 'Error Code';
-			elseif ($CurrentErrorVar == 'L_SHORTMESSAGE')
-				$CurrentVarName = 'Short Message';
-			elseif ($CurrentErrorVar == 'L_LONGMESSAGE')
-				$CurrentVarName == 'Long Message';
-			elseif ($CurrentErrorVar == 'L_SEVERITYCODE')
-				$CurrentVarName = 'Severity Code';
-
-			echo $CurrentVarName . ': ' . $CurrentErrorVal . '<br />';
-		}
-		echo '<br />';
-	}
-}
-
-
-
-
-
--- gateways/evertec/init.php
+++ gateways/evertec/init.php
@@ -4,7 +4,7 @@ add_action('action_hook_espresso_display_onsite_payment_footer','espresso_displa
 event_espresso_require_gateway("evertec/evertec_vars.php");
 
 
-// This is for the transaction processing
+// This is for the transaction processing from teh thank you page
 if(!empty($_REQUEST['evertec'])){
 	event_espresso_require_gateway("evertec/DoDirectPayment.php");
 	add_filter('filter_hook_espresso_transactions_get_attendee_id', 'espresso_transactions_evertec_get_attendee_id');
@@ -15,8 +15,3 @@ function espresso_evertec_formal_name( $gateway_formal_names ) {
 	$gateway_formal_names['evertec'] = 'EverTec';
 }
 add_filter( 'action_hook_espresso_gateway_formal_name', 'espresso_evertec_formal_name', 10, 1 );
-
-//function espresso_evertec_payment_type( $gateway_payment_types ) {
-//	$gateway_payment_types['evertec'] = 'EverTec';
-//}
-//add_filter( 'action_hook_espresso_gateway_payment_type', 'espresso_evertec_payment_type', 10, 1 );
