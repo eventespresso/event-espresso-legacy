db21afe8174b66e1d711c8e21650e9f858870396
Michael Nelson <michael@eventespresso.com>
2014-01-24 10:22:10 -0800

    [touch:3346]{t:15}bumped version AHEAD so client wont update until evertec in master branch

--- espresso.php
+++ espresso.php
@@ -1,12 +1,12 @@
 <?php
 /*
-  Plugin Name: Event Espresso
+  Plugin Name: Event Espresso (evertec)
   Plugin URI: http://eventespresso.com/
   Description: Out-of-the-box Events Registration integrated with PayPal IPN for your WordPress blog/website. <a href="admin.php?page=support" >Support</a>
 
   Reporting features provide a list of events, list of attendees, and excel export.
 
-  Version: 3.1.36.3.P
+  Version: 3.1.37.0.P
 
   Author: Event Espresso
   Author URI: http://www.eventespresso.com
@@ -31,7 +31,7 @@
 //Define the version of the plugin
 function espresso_version() {
 	do_action('action_hook_espresso_log', __FILE__, __FUNCTION__, '');
-	return '3.1.36.3.P';
+	return '3.1.37.0.P';
 }
 
 define("EVENT_ESPRESSO_VERSION", espresso_version());
