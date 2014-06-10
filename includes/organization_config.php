a6d95ac38f4b7f4fee0c9d8366c88054b3a55ab9
Josh Feck <josh@nxnweb.com>
2014-06-10 16:24:38 -0400

    Add Lithuanian currency to the org options selector [touch: 5900]
    Beanstream, PayPal Pro, and 2Checkout appear to support this currency

--- includes/organization_config.php
+++ includes/organization_config.php
@@ -111,6 +111,8 @@ function organization_config_mnu() {
 				break;
 			case 'VEN' : $org_options['currency_symbol'] = 'BsF'; //venezuelan bolivar, although technically its symbol should be VEF
 				break;
+			case 'LTU' : $org_options['currency_symbol'] = 'LT'; // Lithuanian Litas (LTL)
+				break;
 			case 'AUT' : case 'BEL' : case 'CYP' : case 'EST' : case 'FIN' : case 'FRA' : case 'DEU' : case 'GRC' : case 'IRL' : case 'ITA' : case 'LUX' : case 'LVA' : case 'MLT' : case 'NLD' : case 'PRT' : case 'SVK' : case 'SVN' : case 'ESP' : case 'AND' : case 'MCO' : case 'SMR' : case 'VAT' | 'MYT' : case 'MNE' : case 'XKV' : case 'SPM' : $org_options['currency_symbol'] = 'EUR'; // use the Euro for all eurozone countries
 				break;
 			default: $org_options['currency_symbol'] = '$';
