4739a352d33ca186b3cc7171990d6fac827fe3db
Tony Warwick <tony@eventespresso.com>
2015-03-10 17:12:12 +0000

    Remove the amount due from the pending approval page template and add further explanation of how the approval will work.
    [touch: 7692]

--- templates/pending_approval.php
+++ templates/pending_approval.php
@@ -2,11 +2,12 @@
 <?php //Pending Approval Page Template ?>
 	<h3 class="<?php espresso_template_css_class('section_heading','section-heading ui-widget-header ui-corner-top'); ?> "><?php echo $fname ?>,</h3>
 	<div class="<?php espresso_template_css_class('event_data_display','event-data-display ui-widget-content ui-corner-bottom'); ?>" >
-		<p class="<?php espresso_template_css_class('instruct','instruct'); ?>"><?php _e('Your registration is not complete until admin approves.', 'event_espresso'); ?></p>
-		<p>
-			<span class="<?php espresso_template_css_class('section_title','event_espresso_name section-title'); ?>"><?php _e('Amount due: ', 'event_espresso'); ?></span> 
-			<span class="<?php espresso_template_css_class('value','event_espresso_value'); ?>"><?php echo $org_options['currency_symbol']?><?php echo $event_cost; ?></span>
-		</p>
+		<p class="<?php espresso_template_css_class('section_title','event_espresso_name section-title'); ?>"><?php _e('Your registration is not complete until admin approves.', 'event_espresso'); ?></p>
+		
+		<p class="<?php espresso_template_css_class('instruct','instruct'); ?>">
+			<?php _e('Billing will only occur after the attendee has been approved by the event organizer. You will be notified when your registration has been processed. If this is a free event, then no billing will occur.', 'event_espresso'); ?>
+		</p> 
+		
 		<p><span class="<?php espresso_template_css_class('section_title','section-title'); ?>"><?php _e('Your Registration ID:', 'event_espresso'); ?> </span><?php echo $registration_id ?></p>
 	</div>	
 </div>
