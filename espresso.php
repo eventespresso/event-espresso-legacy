ab4bdae01191e264234e2629c5625651356c60e0
Josh Feck <josh@nxnweb.com>
2015-04-23 21:33:53 -0400

    micro version bump [touch: 7987]

--- espresso.php
+++ espresso.php
@@ -6,7 +6,7 @@
 
   Reporting features provide a list of events, list of attendees, and excel export.
 
-  Version: 3.1.37.1.P
+  Version: 3.1.37.2.rc.001
 
   Author: Event Espresso
   Author URI: http://www.eventespresso.com
@@ -31,7 +31,7 @@
 //Define the version of the plugin
 function espresso_version() {
 	do_action('action_hook_espresso_log', __FILE__, __FUNCTION__, '');
-	return '3.1.37.1.P';
+	return '3.1.37.2.rc.001';
 }
 
 define("EVENT_ESPRESSO_VERSION", espresso_version());
