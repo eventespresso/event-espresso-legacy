82d7e5521cd25dca5a1175024d34a4390577a8a9
Seth <seth@eventespresso.com>
2015-04-29 11:14:24 -0600

    Updated pricing.

--- includes/admin_support.php
+++ includes/admin_support.php
@@ -204,8 +204,7 @@ function event_espresso_support() {
 										<dt><a href="http://www.appthemes.com/cp/go.php?r=12413&i=l0" target="_blank">AppThemes</a></dt>
 										<dd>AppThemes are built for businesses of all sizes and run on WordPress so  you don't have to worry about the headache of setting up a complex  server environment.</dd>
 										<dt><a href="http://www.woothemes.com/amember/go.php?r=28039&amp;i=b16" target="_blank">WooThemes</a></dt>
-										<dd>Top quality, cutting edge WordPress themes and superior customer support.</dd>
-										<dt><a href="http://www.shareasale.com/r.cfm?B=258340&amp;U=471857&amp;M=27087" target="_blank">PageLines</a></dt>
+										
 										<dd>Build websites faster and better using drag and drop, layout, typography, design-control and more... </dd>
 										<dt><a href="http://www.studiopress.com/" target="_blank">StudioPress</a></dt>
 										<dd>When you buy a StudioPress theme, you are getting state of the art code,  smart design architecture as well as an array of beautiful frames for  your content.</dd>
