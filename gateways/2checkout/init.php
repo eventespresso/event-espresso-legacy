77f982eea4ca1da23866bab5a042f8b41eb619e9
Josh Feck <josh@nxnweb.com>
2015-09-17 13:10:42 -0400

    change name of request var from "type", which doesn't get returned to "payment_method" and simplify the return URL [touch: 8844]

--- gateways/2checkout/init.php
+++ gateways/2checkout/init.php
@@ -5,7 +5,7 @@ add_action('action_hook_espresso_display_offsite_payment_footer', 'espresso_disp
 event_espresso_require_gateway("2checkout/2checkout_vars.php");
 
 // This is for the return from 2Checkout's servers
-if (!empty($_REQUEST['type']) && $_REQUEST['type'] == '2checkout') {
+if (!empty($_REQUEST['payment_method']) && $_REQUEST['payment_method'] == '2checkout') {
 	event_espresso_require_gateway("2checkout/2checkoutpaymentprocess.php");
 	add_filter('filter_hook_espresso_transactions_get_attendee_id', 'espresso_transactions_2checkout_get_attendee_id');
 	add_filter('filter_hook_espresso_thank_you_get_payment_data', 'espresso_process_2checkout');
