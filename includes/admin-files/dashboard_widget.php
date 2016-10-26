8086801043bda24226609cbc70e8c77e8d64254c
Josh Feck <josh@nxnweb.com>
2016-10-26 17:33:48 -0400

    add capability check for upcoming events widget, add some brackets to the if statement [touch: 10200]

--- includes/admin-files/dashboard_widget.php
+++ includes/admin-files/dashboard_widget.php
@@ -116,8 +116,10 @@ add_action('wp_dashboard_setup', 'event_espresso_dashboard_widget');
 
 function event_espresso_dashboard_widget() {
 	global $wp_meta_boxes, $org_options;
-	if (!isset($org_options['espresso_dashboard_widget'])||$org_options['espresso_dashboard_widget'] != 'Y')
+	$user_access = apply_filters( 'filter_hook_espresso_event_dashboard_widget', current_user_can('administrator') );
+	if (!isset($org_options['espresso_dashboard_widget'])||$org_options['espresso_dashboard_widget'] != 'Y' || !$user_access) {
 		return;
+	}
 	wp_enqueue_style('event_espresso', EVENT_ESPRESSO_PLUGINFULLURL . 'css/admin-styles.css');
 	wp_enqueue_script( 'dataTables',  EVENT_ESPRESSO_PLUGINFULLURL.'scripts/jquery.dataTables.min.js', array('jquery') );//Events core table script
 	wp_add_dashboard_widget('todays_events_widget', __('Upcoming Events', 'event_espresso'), 'custom_dashboard_events');
