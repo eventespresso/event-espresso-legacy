1a553db57b6417de0ec946a247a85c56aa9d6dd8
Tony Warwick <tony@eventespresso.com>
2019-10-03 11:05:05 +0100

    Switch over to use a payment intent object when processing the payment; Update the registration based on the payment intent object; If we have an payment intent ID use that to pull the objects from Stripe;

--- gateways/stripe/do_transaction.php
+++ gateways/stripe/do_transaction.php
@@ -13,69 +13,62 @@ function espresso_process_stripe($payment_data) {
 	$payment_data['txn_type'] = 'Stripe';
 	$payment_data['payment_status'] = 'Incomplete';
 	$stripe_settings = get_option('event_espresso_stripe_settings');
+	$intent = null;
+	$intent_array = null;
 	
 	//Check for alternate Stripe settings
 	if ( !empty($payment_data['event_meta']['stripe_secret_key'] ) ) {
 		//Alternative Stripe settings set on the event, set the key to be used here.
-    	$stripe_settings['stripe_secret_key'] = $payment_data['event_meta']['stripe_secret_key'];
+		$stripe_settings['stripe_secret_key'] = $payment_data['event_meta']['stripe_secret_key'];
 	}
 
 	//Include the Stripe API
-	require_once (dirname(__FILE__).'/stripe-php-1.18.0/lib/Stripe.php');
+	require_once (dirname(__FILE__).'/stripe-php-6.43.1/init.php');
 
-	//Pull the eeStripeToken posted by Checkout.
-	$token = $_POST['eeStripeToken'];
+	\Stripe\Stripe::setApiKey($stripe_settings['stripe_secret_key']);
 
-	//Set the Stripe API secret key.
-	Stripe::setApiKey( $stripe_settings['stripe_secret_key'] );
-    
-    //Build the Stripe data array
-    $stripe_data = array(
-        'amount' => str_replace( array(',', '.'), '', number_format( $payment_data['total_cost'], espresso_get_stripe_decimal_places($stripe_settings['stripe_currency_symbol'])) ),
-        'currency' => !empty($stripe_settings['stripe_currency_symbol']) ? $stripe_settings['stripe_currency_symbol'] : 'USD',
-        'card' => $token,
-        'description' =>  sprintf(
-        	/* translators: 1: event name, 2: event date, 3: attendee first name, 4: attendee last name */
-            esc_html__('%1$s [%2$s] >> %3$s %4$s', 'event_espresso'),
-            $payment_data["event_name"],
-            date('m-d-Y', strtotime($payment_data['start_date'])),
-            $payment_data["fname"],
-            $payment_data["lname"]
-        )
-    );
-	
-	//Create a charge with Stripe.
-	$charge = Stripe_Charge::create( $stripe_data );
+	// The JS set the payment intend ID in a hidden field, so pull that value. 
+	$payment_intent_id = isset($_REQUEST['espresso_stripe_payment_intent_id']) ? $_REQUEST['espresso_stripe_payment_intent_id'] : '';
 
-	//Convert the Stripe charge to an array so we can use it.
-	$charge_array = $charge->__toArray(true);
-	
-	if ( !empty( $charge_array ) ) {
-		$payment_data['txn_details'] = serialize( $charge_array );
-		if ( isset($charge_array['status'] ) ) {
+	// Pull the payment intent object from Stripe.
+	if ($payment_intent_id) {
+		$intent = \Stripe\PaymentIntent::retrieve(
+			$payment_intent_id
+		);
+		//Convert the Stripe payment intent to an array so we can use it.
+		$intent_array = $intent->__toArray(true);
+	}
+
+	// Check we have values from the intent object in an array.
+	if ( !empty( $intent_array ) ) {
+		$payment_data['txn_details'] = serialize( $intent_array );
+		if ( isset($intent_array['status'] ) ) {
+			$stripe_charge = reset($intent_array['charges']['data']);
 			echo "<div id='stripe_response'>";
-			if ($charge_array['paid'] === true) {
-				echo "<div class='stripe_status'>" . $charge_array['outcome']['seller_message'] . "</div>";
+			if ($intent_array['status'] === 'succeeded') {
+				if(!empty($stripe_charge)){
+					echo "<h3 class='stripe_status'>" . $stripe_charge['outcome']['seller_message'] . "</h3>";
+				}
 				$payment_data['payment_status'] = 'Completed';
-				$payment_data['txn_id'] = $charge_array['id'];
+				$payment_data['txn_id'] = $intent_array['id'];
 			}
-			if ( !empty($charge_array['failure_code']) ) {
-				echo "<div class='stripe_error'>" . 
+			if ( !empty($stripe_charge['failure_code']) ) {
+				echo "<h3 class='stripe_error'>" . 
 					sprintf(
-			        	/* translators: 1: error code, 2: failure message */
-			            esc_html__('ERROR: %1$s - %2$s', 'event_espresso'),
-			            $charge_array['failure_code'],
-			            $charge_array['failure_message']
+						/* translators: 1: error code, 2: failure message */
+						esc_html__('ERROR: %1$s - %2$s', 'event_espresso'),
+						$intent_array['failure_code'],
+						$intent_array['failure_message']
 
-			        )  . 
-			    	"</div>";
+					)  . 
+					"</h3>";
 			}
 			echo "</div>";
 		}
 	}
-	if ( empty($charge_array['status'] ) ) {
+	if ( empty($intent) ) {
 		echo "<div id='stripe_response' class='stripe_error'>Looks like something went wrong.  Please try again or notify the website administrator.</div>";
 	}
-	//add_action('action_hook_espresso_email_after_payment', 'espresso_email_after_payment');
+	add_action('action_hook_espresso_email_after_payment', 'espresso_email_after_payment');
 	return $payment_data;
 }
