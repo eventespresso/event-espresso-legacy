9be678cae2b9634e33bc89d1f93bfc0ec4b8115e
Seth <seth@eventespresso.com>
2019-10-08 11:22:55 -0600

    Update includes/category-management/add_cat_to_db.php
    Co-Authored-By: Mike Nelson <michael@eventespresso.com>

--- includes/category-management/add_cat_to_db.php
+++ includes/category-management/add_cat_to_db.php
@@ -5,7 +5,7 @@ function add_cat_to_db(){
 		$category_name			= isset($_REQUEST['category_name']) && !empty($_REQUEST['category_name']) ? esc_html($_REQUEST['category_name']) : '';
 		$category_identifier	= isset($_REQUEST['category_identifier']) && !empty($_REQUEST['category_identifier']) ? $category_identifier = sanitize_title_with_dashes($_REQUEST['category_identifier']) :  $category_identifier = sanitize_title_with_dashes($category_name.'-'.time()) ;
 		$category_desc			= isset($_REQUEST['category_desc']) && !empty($_REQUEST['category_desc']) ? wp_kses_post( $_REQUEST['category_desc'] ) : ''; 
-		$display_category_desc	= isset($_REQUEST['display_desc']) && !empty($_REQUEST['display_desc']) ? sanitize_text_field($_REQUEST['display_desc']) : '';
+		$display_category_desc	= isset($_REQUEST['display_desc']) && $_REQUEST['display_desc'] === 'Y' ? 'Y' : 'N';
 		
 		if (!function_exists('espresso_member_data'))
 			$current_user->ID = 1;
